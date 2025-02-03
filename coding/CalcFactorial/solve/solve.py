1c6362110254a7702fd5e5151a93fe4b58197536
Paulatsya Vekaria <74497266+Paulatsya@users.noreply.github.com>
2025-02-03 14:53:55 +0530

    updates

---
+++ coding/CalcFactorial/solve/solve.py
@@ -0,0 +1,13 @@
+from pwn import *
+import math
+
+io = remote('localhost', 54319)  
+
+for i in range(10):  # 10 rounds
+    io.readline()  
+    io.readuntil(b'Calculate the factorial of: ')
+    num = int(io.readline().strip())
+    fact = math.factorial(num)
+    io.sendline(str(fact).encode())
+
+io.interactive()  
